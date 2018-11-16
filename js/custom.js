2e52de8dc0b6f35fc82b1c39665a3e1852e76eff
Marwan El-Adawy <marwan.mahmoud@cis.asu.edu.eg>
2018-11-16 15:54:31 +0200

    companytemp

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+﻿
+// add now class to li item when click
+$(document).ready(function () {
+    $('li').click(function () {
+        $(this).addClass('now').siblings().removeClass('now');;
+
+    })
+})
+
+// make smooth scroll 
+
+$(document).ready(function () {
+    $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+        if (this.hash !== "") {
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 900, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+// animation with woe .js
+
+new WOW().init();
+
+// nice scroll
+$("html").niceScroll({
+    cursorcolor: "#089b4a"
+
+});
